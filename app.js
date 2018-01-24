8d9ebcae659b0273f7a14f1ff9eb2b6c78971620
Erik Sidnei Corrêa <Erik562@hotmail.com>
2018-01-24 21:53:30 -0200

    adicionando bjs

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 express = require('express');
 app = express();
-var head = "<!DOCTYPE html>";
-head += "<html lang='en'>";
-head += "<head>";
-head += "<meta charset='UTF-8'>";
-head += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
-head += "<title>Document</title>";
-head += "</head>";
-dados = "<body>";
-  
-footer = "</body>";
-footer += "</html>";
+app.set('view engine','ejs');
+app.get('/tecnologia', (req,res) => res.render('secao/tecnologia'));
+
 app.get('/',(req,res)=> res.send(head+"Portal de noticias"+footer));
-app.get('/tecnologia', (req,res) => res.send(head+"Noticias de tecnologia"+footer))
 app.listen('3000', () => console.log('Servidor rodando com Express'));
